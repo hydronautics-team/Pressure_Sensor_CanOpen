a0629314394b5bac6c009cf6b7992b109e100352
Nikita Bulatov <n.bulatov@1440.space>
2026-03-21 16:51:15 +0300

    Гидропротокол на подмодулях

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -1,5 +1,5 @@
 #ifdef __cplusplus
- extern "C" {
+extern "C" {
 #endif
 
 #include "main.h"
@@ -8,13 +8,49 @@
 }
 #endif
 
+#include "hydrv_gpio_low.hpp"
+#include "hydrv_rs_485.hpp"
 #include "pressure_wrapper.h"
+#include "hydrolib_bus_datalink_stream.hpp"
+#include "hydrolib_bus_application_master.hpp"
+
+
+#define BUFFER_LENGTH 5 // TODO:make variable
+
+constinit hydrv::GPIO::GPIOLow rx_pin(hydrv::GPIO::GPIOLow::GPIOA_port, 10,
+                                      hydrv::GPIO::GPIOLow::GPIO_UART_RX);
+
+constinit hydrv::GPIO::GPIOLow tx_pin(hydrv::GPIO::GPIOLow::GPIOA_port, 9,
+                                      hydrv::GPIO::GPIOLow::GPIO_UART_TX);
+
+constinit hydrv::GPIO::GPIOLow direction_pin(hydrv::GPIO::GPIOLow::GPIOA_port,
+                                             8,
+                                             hydrv::GPIO::GPIOLow::GPIO_Output);
+
+static constexpr hydrv::UART::UARTLow::UARTPreset USART1_115200_LOW{
+    USART1_BASE,
+    7,
+    RCC_APB2ENR_USART1EN,
+    RCC_BASE + offsetof(RCC_TypeDef, APB2ENR),
+    USART1_IRQn,
+    39,
+    1};
+
+constinit hydrv::RS485::RS485<255, 255> RS(USART1_115200_LOW, rx_pin, tx_pin,
+                                           direction_pin, 7);
+class Logger {
+public:
+  Logger() = default;
+};
+Logger loger;
+hydrolib::bus::datalink::StreamManager manager(1, RS, loger);
+hydrolib::bus::datalink::Stream stream(manager, 2);
+hydrolib::bus::application::Master master(stream, loger);
 
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc1;
 DMA_HandleTypeDef hdma_adc1;
 TIM_HandleTypeDef htim3;
-UART_HandleTypeDef huart1;
 
 PressureWrapper sensor(hadc1, htim3);
 
@@ -22,11 +58,10 @@ PressureWrapper sensor(hadc1, htim3);
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_ADC1_Init(void);
 static void MX_TIM3_Init(void);
-static void MX_USART1_UART_Init(void);
+uint32_t depth;
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -64,19 +99,18 @@ int main(void) {
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
-  MX_GPIO_Init();
   MX_DMA_Init();
   MX_ADC1_Init();
   MX_TIM3_Init();
-  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
   sensor.init();
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET);
-
+  RS.Init();
+  
   while (1) {
-    
+    manager.Process();
     sensor.poll();
-
+    depth = sensor.getDepthMm();
+    master.Write(static_cast<void*>(&depth), 0, sizeof(depth));
   }
   /* USER CODE END 3 */
 }
@@ -215,36 +249,6 @@ static void MX_TIM3_Init(void) {
 }
 
 /**
- * @brief USART1 Initialization Function
- * @param None
- * @retval None
- */
-static void MX_USART1_UART_Init(void) {
-
-  /* USER CODE BEGIN USART1_Init 0 */
-
-  /* USER CODE END USART1_Init 0 */
-
-  /* USER CODE BEGIN USART1_Init 1 */
-
-  /* USER CODE END USART1_Init 1 */
-  huart1.Instance = USART1;
-  huart1.Init.BaudRate = 115200;
-  huart1.Init.WordLength = UART_WORDLENGTH_8B;
-  huart1.Init.StopBits = UART_STOPBITS_1;
-  huart1.Init.Parity = UART_PARITY_NONE;
-  huart1.Init.Mode = UART_MODE_TX_RX;
-  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart1) != HAL_OK) {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART1_Init 2 */
-
-  /* USER CODE END USART1_Init 2 */
-}
-
-/**
  * Enable DMA controller clock
  */
 static void MX_DMA_Init(void) {
@@ -258,36 +262,6 @@ static void MX_DMA_Init(void) {
   HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
 }
 
-/**
- * @brief GPIO Initialization Function
- * @param None
- * @retval None
- */
-static void MX_GPIO_Init(void) {
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  /* USER CODE BEGIN MX_GPIO_Init_1 */
-
-  /* USER CODE END MX_GPIO_Init_1 */
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin : PA8 */
-  GPIO_InitStruct.Pin = GPIO_PIN_8;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /* USER CODE BEGIN MX_GPIO_Init_2 */
-
-  /* USER CODE END MX_GPIO_Init_2 */
-}
-
 /* USER CODE BEGIN 4 */
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc) {
   sensor.setFullConvFlag();
@@ -297,10 +271,7 @@ void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef *hadc) {
   sensor.setHalfConvFlag();
 }
 
-void HAL_UART_TxCpltCallback(UART_HandleTypeDef *UartHandle) {
-  if (UartHandle == &huart1) {
-  }
-}
+void USART1_IRQHandler(void) { RS.IRQCallback(); }
 /* USER CODE END 4 */
 
 /**
--- Core/Src/stm32f1xx_it.c
+++ Core/Src/stm32f1xx_it.c
@@ -243,19 +243,7 @@ void TIM3_IRQHandler(void)
   /* USER CODE END TIM3_IRQn 1 */
 }
 
-/**
-  * @brief This function handles USART1 global interrupt.
-  */
-void USART1_IRQHandler(void)
-{
-  /* USER CODE BEGIN USART1_IRQn 0 */
 
-  /* USER CODE END USART1_IRQn 0 */
-  HAL_UART_IRQHandler(&huart1);
-  /* USER CODE BEGIN USART1_IRQn 1 */
-
-  /* USER CODE END USART1_IRQn 1 */
-}
 
 /* USER CODE BEGIN 1 */
 
